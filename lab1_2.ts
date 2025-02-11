70342954ee18750a8059708bfd90965fdb2617b2
sk1fix <agafonovtoha427@gmail.com>
2025-02-11 20:18:41 +0400

    lab1-2

---
+++ lab1_2.ts
@@ -0,0 +1,11 @@
+const int: number = 1;
+const str: string = '1';
+const c: boolean = true;
+
+let arr: number[] = [1, 2, 3, 4, 5];
+let double: {first: string, second: number} = {
+    first: "first",
+    second: 2
+};
+
+console.log(int, str, c, arr, double)

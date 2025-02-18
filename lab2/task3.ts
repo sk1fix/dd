b788c1ed4ff3f487a1505363edbc996f628138f3
sk1fix <agafonovtoha427@gmail.com>
2025-02-18 14:49:40 +0400

    l2 t3

---
+++ lab2/task3.ts
@@ -0,0 +1,10 @@
+enum Balls {
+    Football = "Футбольный мяч",
+    Basketball = "Баскетбольный мяч",
+    Tennis = "Теннисный мяч",
+    Volleyball = "Волейбольный мяч",
+    Baseball = "Бейсбольный мяч",
+    Golf = "Мяч для гольфа"
+}
+
+console.log(Balls.Volleyball);

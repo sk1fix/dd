1fb3025e95269cdea156d16eb352598adc82385d
sk1fix <agafonovtoha427@gmail.com>
2025-03-25 15:11:39 +0400

    lab1

---
+++ mod3/lab1/index.ts
@@ -0,0 +1,21 @@
+function loadCars(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.json', true);
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            console.log('Данные загружены:', data);
+        } else {
+            console.error('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+        }
+    };
+    xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('loadButton');
+    if (button) {
+        button.addEventListener('click', loadCars);
+    }
+});

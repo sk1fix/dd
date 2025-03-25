77b9c69442009bdddc954776d9e9715eeaf4b6d6
sk1fix <agafonovtoha427@gmail.com>
2025-03-25 20:50:11 +0400

    lab2

--- mod3/lab2/digital-department-application/src/app/app.component.ts
+++ mod3/lab2/digital-department-application/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'digital-department-application';
+
+  greeting = "";
+
+  onSayHi(): void {
+    alert("Привет, " + this.greeting);
+  }
 }
